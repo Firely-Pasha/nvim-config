c3d4e9950bf60d4cf58ca8608399e9ee06bd3403
Pavel Z <tiddiebiter@gmail.com>
2022-10-29 17:07:23 +0500

    Init

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("vincentdian.plugins-setup")
+
+require("vincentdian.core.colorscheme")
+require("vincentdian.core.options")
+require("vincentdian.core.keymaps")
+
+require("vincentdian.plugins.lualine")
+require("vincentdian.plugins.telescope")
+require("vincentdian.plugins.nvim-tree")
---
+++ lua/vincentdian/plugins/lualine.lua
@@ -0,0 +1,27 @@
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local theme = require("lualine.themes.onelight")
+
+lualine.setup({
+  options = {
+    theme = theme
+  }
+})
