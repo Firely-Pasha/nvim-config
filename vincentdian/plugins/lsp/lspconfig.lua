70cdb7484e3688e0c16f6d88bfb749e968f0f2af
Pavel Z <tiddiebiter@gmail.com>
2023-07-13 06:57:57 +0500

    Update

--- vincentdian/plugins/lsp/lspconfig.lua
+++ vincentdian/plugins/lsp/lspconfig.lua
@@ -25,10 +25,11 @@ local on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 
 	-- set keybinds
-	keymap.set("n", "<leader>lf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	keymap.set("n", "<leader>lf", "<cmd>Lspsaga finder<CR>", opts) -- show definition, references
 	keymap.set("n", "<leader>gd", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
 	keymap.set("n", "<leader>le", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "<leader>ge", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts) -- go to implementation
 	keymap.set("n", "<leader>lc", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	keymap.set("n", "<leader>lr", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
 	keymap.set("n", "<leader>ds", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
